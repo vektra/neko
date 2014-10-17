470bf7180e096b44a22d64298b93674b040f6046
Evan Phoenix <evan@phx.io>
2014-10-17 11:24:30 -0700

    Add Cleanup and Only

--- neko.go
+++ neko.go
@@ -18,9 +18,11 @@ type test struct {
 type Organizer struct {
 	t *testing.T
 
-	mocks []*mock.Mock
-	setup []func()
-	tests []test
+	only    *test
+	mocks   []*mock.Mock
+	setup   []func()
+	cleanup []func()
+	tests   []test
 }
 
 // Create a new Organizer against testing's T interface
@@ -38,9 +40,18 @@ func (o *Organizer) Setup(f func()) {
 	o.setup = append(o.setup, f)
 }
 
+// Add some work to be done after each test
+func (o *Organizer) Cleanup(f func()) {
+	o.cleanup = append(o.cleanup, f)
+}
+
 // Add a test.
 func (o *Organizer) It(name string, f func()) {
 	o.tests = append(o.tests, test{name, f})
+}
+
+func (o *Organizer) Only(name string, f func()) {
+	o.only = &test{name, f}
 }
 
 // Useful by allowing the developer to simply add 'N' before
@@ -51,28 +62,45 @@ func (o *Organizer) NIt(name string, f func()) {
 
 // Coordinate running the tests with the setups and mocks
 func (o *Organizer) Run() {
+	if o.only != nil {
+		o.runTest(o.only)
+		return
+	}
+
 	for _, test := range o.tests {
-		if test.Func == nil {
-			o.t.Logf("==== DISABLED: %s ====", test.Name)
-			continue
-		}
+		o.runTest(&test)
+	}
+}
 
-		o.t.Logf("==== %s ====", test.Name)
+func (o *Organizer) runTest(test *test) {
+	if test.Func == nil {
+		o.t.Logf("==== DISABLED: %s ====", test.Name)
+		return
+	}
 
-		for _, mock := range o.mocks {
-			mock.ExpectedCalls = nil
-			mock.Calls = nil
-		}
+	o.t.Logf("==== %s ====", test.Name)
 
-		for _, setup := range o.setup {
-			setup()
-		}
+	for _, mock := range o.mocks {
+		mock.ExpectedCalls = nil
+		mock.Calls = nil
+	}
 
-		test.Func()
+	for _, setup := range o.setup {
+		setup()
+	}
 
-		for _, mock := range o.mocks {
-			mock.AssertExpectations(o.t)
-		}
+	defer o.runCleanup()
+
+	test.Func()
+
+	for _, mock := range o.mocks {
+		mock.AssertExpectations(o.t)
+	}
+}
+
+func (o *Organizer) runCleanup() {
+	for _, cleanup := range o.cleanup {
+		cleanup()
 	}
 }
 
